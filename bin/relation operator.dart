6f3364078c80599f0bbdd6ff688af4e1a7dd5eb3
ANUPRIYA <anupriyapradeepanus@gmail.com>
2023-08-10 13:56:07 +0530

    commit

---
+++ bin/relation operator.dart
@@ -0,0 +1,11 @@
+
+void main()
+{
+  int age = 25;  // ANSWER ALWAYS TURE OR FALSE
+  print(age>18);
+  print(age<18);
+  print(age>=18);
+  print(age<=18);
+  print(age==18);
+  print(age!=18);
+}
---
+++ bin/unery operator.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  int x=1;
+  print(x++);     //1234554321
+  print(x);
+  print(++x);
+  print(++x);
+  print(++x);
+  print(x);
+  print(--x);
+  print(--x);
+  print(--x);
+  print(--x);
+
+
+}
